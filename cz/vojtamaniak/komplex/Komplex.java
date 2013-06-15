895703feaf3929417dee501a9d81cffda0d3109e
vojtamaniak <vojtamaniak@gmail.com>
2013-06-15 14:43:43 +0200

    Added MessageManager instance to Komplex.java

--- cz/vojtamaniak/komplex/Komplex.java
+++ cz/vojtamaniak/komplex/Komplex.java
@@ -5,19 +5,26 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Komplex extends JavaPlugin {
-  
+	
 	private Logger log;
+	private MessageManager msgManager;
 	
 	@Override
 	public void onEnable(){
 		log = getLogger();
-		
+		msgManager = new MessageManager(this);
 		
 		log.info("is enabled.");
+		
+		msgManager.init();
 	}
 	
 	@Override
 	public void onDisable(){
 		log.info("is disabled.");
+	}
+	
+	public MessageManager getMessageManager(){
+		return msgManager;
 	}
 }
