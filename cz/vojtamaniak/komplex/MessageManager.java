cf94e923b8dd3c0c9aaf761e3033b8d10c7ea016
vojtamaniak <vojtamaniak@gmail.com>
2013-06-15 18:43:26 +0300

    Added messages for command "feed" + add saving the file.

--- cz/vojtamaniak/komplex/MessageManager.java
+++ cz/vojtamaniak/komplex/MessageManager.java
@@ -1,12 +1,13 @@
 package cz.vojtamaniak.komplex;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class MessageManager {
-  
+	
 	private Komplex plg;
 	private File messFile;
 	private FileConfiguration mess;
@@ -24,7 +25,17 @@ public class MessageManager {
 	public void init(){
 		if(!messFile.exists()){
 			mess.set("NO_PERMISSION", "&dNemas prava.");
+			mess.set("FEED_SELF", "&7Nasytil ses.");
+			mess.set("FEED_OTHER_WHISPER", "&7Nasytil te admin %NICK%.");
+			mess.set("FEED_OTHER", "&7Nasytil jsi hrace %NICK%.");
+			mess.set("PLAYER_OFFLINE", "&dTento hrac je offline.");
 			//There it will be filled with another messages.
+			
+			try {
+				mess.save(messFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
