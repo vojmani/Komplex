0d21a70699a6443af34e7566b26e61babe5af989
vojtamaniak <vojtamaniak@gmail.com>
2013-06-16 15:15:56 +0300

    A few changes in MessageManager.java
    1. Added Logger.
    2. Method init() now checks if messages.yml file contains Messages. If not, it will create missing message(s).
    3. Edited comment in String format :D

--- cz/vojtamaniak/komplex/MessageManager.java
+++ cz/vojtamaniak/komplex/MessageManager.java
@@ -2,6 +2,7 @@ package cz.vojtamaniak.komplex;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -11,11 +12,13 @@ public class MessageManager {
 	private Komplex plg;
 	private File messFile;
 	private FileConfiguration mess;
+	private Logger log;
 	
 	public MessageManager(Komplex plg){
 		this.plg = plg;
 		this.messFile = new File(plg.getDataFolder(), "messages.yml");
 		this.mess = YamlConfiguration.loadConfiguration(messFile);
+		this.log = plg.getLogger();
 	}
 	
 	public String getMessage(String message){
@@ -23,14 +26,30 @@ public class MessageManager {
 	}
 	
 	public void init(){
-		if(!messFile.exists()){
+		boolean allOk = true;
+		if(!mess.contains("NO_PERMISSION")){
 			mess.set("NO_PERMISSION", "&dNemas prava.");
+			allOk = false;
+		}
+		if(!mess.contains("FEED_SELF")){
 			mess.set("FEED_SELF", "&7Nasytil ses.");
+			allOk = false;
+		}
+		if(!mess.contains("FEED_OTHER_WHISPER")){
 			mess.set("FEED_OTHER_WHISPER", "&7Nasytil te admin %NICK%.");
+			allOk = false;
+		}
+		if(!mess.contains("FEED_OTHER")){
 			mess.set("FEED_OTHER", "&7Nasytil jsi hrace %NICK%.");
-			mess.set("PLAYER_OFFLINE", "&dTento hrac je offline.");
+			allOk = false;
+		}
+		if(!mess.contains("PLAYER_OFFLINE")){
+			mess.set("PLAYER_OFFLINE", "&dTento hrac neni pripojen.");
+			allOk = false;
+		}
 			//There it will be filled with another messages.
-			
+		if(!allOk){
+			log.info("repairing or creating new messages.yml file.");
 			try {
 				mess.save(messFile);
 			} catch (IOException e) {
@@ -40,7 +59,7 @@ public class MessageManager {
 	}
 	
 	private String format(String message){
-		//Change & char with §
+		//Change & character with §
 		return message.replaceAll("&", "§");
 	}
 }
