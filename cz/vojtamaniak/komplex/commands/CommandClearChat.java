26c7d7e3a3e74fda663e042bade9767b9a0e16a2
vojtamaniak <vojtamaniak@gmail.com>
2013-06-19 20:44:16 +0300

    Added class CommandClearChat.java for executing command "clearchat"

---
+++ cz/vojtamaniak/komplex/commands/CommandClearChat.java
@@ -0,0 +1,37 @@
+package cz.vojtamaniak.komplex.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import cz.vojtamaniak.komplex.Komplex;
+
+public class CommandClearChat extends ICommand implements CommandExecutor {
+
+  public CommandClearChat(Komplex plg) {
+		super(plg);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] arg) {
+		if(cmd.getName().equalsIgnoreCase("clearchat")){
+			if(sender.isOp() || sender.hasPermission("komplex.clearchat")){
+				for(int i=0; i<120; i++){
+					for(Player p : Bukkit.getOnlinePlayers()){
+						p.sendMessage("");
+					}
+				}
+				for(Player p : Bukkit.getOnlinePlayers()){
+					Bukkit.broadcastMessage(msgManager.getMessage("CHAT_CLEARED").replaceAll("%NICK%", sender.getName()));
+				}
+			}else{
+				sender.sendMessage(msgManager.getMessage("NO_PERMISSION"));
+			}
+			return true;
+		}
+		return false;
+	}
+	
+}
