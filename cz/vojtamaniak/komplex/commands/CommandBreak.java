20ab6b877e241079d18ef106401cf64056c14e99
vojtamaniak <vojtamaniak@gmail.com>
2013-06-19 18:25:36 +0300

    Fixed CommandBreak.java

--- cz/vojtamaniak/komplex/commands/CommandBreak.java
+++ cz/vojtamaniak/komplex/commands/CommandBreak.java
@@ -1,29 +1,31 @@
 package cz.vojtamaniak.komplex.commands;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.block.BlockBreakEvent;
 
 import cz.vojtamaniak.komplex.Komplex;
 
 public class CommandBreak extends ICommand implements CommandExecutor {
 
-  public CommandBreak(Komplex plg) {
+	public CommandBreak(Komplex plg) {
 		super(plg);
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] arg) {
-		if(cmd.toString().equalsIgnoreCase("break")){
+		if(cmd.getName().equalsIgnoreCase("break")){
 			if(sender instanceof Player){
 				Player player = (Player) sender;
-				if(player.getItemOnCursor().getType() == Material.BEDROCK){
-					this.breakBedrock(player);
+				Block target = player.getTargetBlock(null, 30);
+				if(target.getType() == Material.BEDROCK){
+					this.breakBedrock(player, target);
 				}else{
-					this.breakOtherBlocks(player);
+					this.breakOtherBlocks(player, target);
 				}
 			}
 			return true;
@@ -31,19 +33,26 @@ public class CommandBreak extends ICommand implements CommandExecutor {
 		return false;
 	}
 	
-	private void breakOtherBlocks(Player player){
+	private void breakOtherBlocks(Player player, Block target){
 		if(player.isOp() || player.hasPermission("komplex.break")){
-			player.setItemOnCursor(new ItemStack(Material.AIR));
+			BlockBreakEvent e = new BlockBreakEvent(target, player);
+			if(!e.isCancelled()){
+				target.setType(Material.AIR);
+			}
+			target.setType(Material.AIR);
 		}else{
 			player.sendMessage(msgManager.getMessage("NO_PERMISSION"));
 		}
 	}
 	
-	private void breakBedrock(Player player){
+	private void breakBedrock(Player player, Block target){
 		if(player.isOp() || player.hasPermission("komplex.break.bedrock")){
-			player.setItemOnCursor(new ItemStack(Material.AIR));
+			BlockBreakEvent e = new BlockBreakEvent(target, player);
+			if(!e.isCancelled()){
+				target.setType(Material.AIR);
+			}
 		}else{
-			player.sendMessage(msgManager.getMessage("NO_PERMISSION"));
+			player.sendMessage(msgManager.getMessage("NO_PERMISSION_BEDROCK"));
 		}
 	}
 }
