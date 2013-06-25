93d4dfcfb2e33543dccf07e5d414f29b097870ca
vojtamaniak <vojtamaniak@gmail.com>
2013-06-25 18:15:54 +0300

    Create IListener.java as superclass for other listeners class.

---
+++ cz/vojtamaniak/komplex/listeners/IListener.java
@@ -0,0 +1,17 @@
+package cz.vojtamaniak.komplex.listeners;
+
+import org.bukkit.event.Listener;
+
+import cz.vojtamaniak.komplex.Komplex;
+import cz.vojtamaniak.komplex.MessageManager;
+
+abstract public class IListener implements Listener {
+  
+	protected MessageManager msgManager;
+	protected Komplex plg;
+	
+	public IListener(Komplex plg){
+		this.msgManager = plg.getMessageManager();
+		this.plg = plg;
+	}
+}
