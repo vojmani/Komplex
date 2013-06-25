98f83c5e0992870926280329c1f614893e3b622e
vojtamaniak <vojtamaniak@gmail.com>
2013-06-25 17:27:08 +0200

    Create onQuit.java

---
+++ cz/vojtamaniak/komplex/listeners/onQuit.java
@@ -0,0 +1,26 @@
+package cz.vojtamaniak.komplex.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.EventHandler;
+
+import cz.vojtamaniak.komplex.Komplex;
+
+public class onQuit extends IListener {
+
+  public onQuit(Komplex plg) {
+		super(plg);
+	}
+	
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onQuit(PlayerQuitEvent e){
+		for(Player p : Bukkit.getOnlinePlayers()){
+			if(p.hasPermission("komplex.messages.onquit.receive")){
+				p.sendMessage(msgManager.getMessage("MESSAGE_QUIT"));
+			}
+		}
+		plg.removeUser(e.getPlayer().getName());
+	}
+}
