058b8000835502fcd823f83c4792cec823317f97
vojtamaniak <vojtamaniak@gmail.com>
2013-06-25 17:11:34 +0200

    Added users list + executor for command "god" + listeners

--- cz/vojtamaniak/komplex/Komplex.java
+++ cz/vojtamaniak/komplex/Komplex.java
@@ -1,21 +1,32 @@
 package cz.vojtamaniak.komplex;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import cz.vojtamaniak.komplex.commands.CommandBreak;
+import cz.vojtamaniak.komplex.commands.CommandClearChat;
 import cz.vojtamaniak.komplex.commands.CommandFeed;
+import cz.vojtamaniak.komplex.commands.CommandFly;
+import cz.vojtamaniak.komplex.commands.CommandGod;
+import cz.vojtamaniak.komplex.listeners.onEntityDamageEvent;
+import cz.vojtamaniak.komplex.listeners.onJoin;
 
 public class Komplex extends JavaPlugin {
 	
 	private Logger log;
 	private MessageManager msgManager;
+	private ConfigManager confManager;
+	private HashMap<String, User> users;
 	
 	@Override
 	public void onEnable(){
 		log = getLogger();
 		msgManager = new MessageManager(this);
+		confManager = new ConfigManager(this);
+		users = new HashMap<String, User>();
 		
 		log.info("is enabled.");
 		
@@ -39,9 +50,21 @@ public class Komplex extends JavaPlugin {
 		getCommand("feed").setExecutor(new CommandFeed(this));
 		getCommand("fly").setExecutor(new CommandFly(this));
 		getCommand("clearchat").setExecutor(new CommandClearChat(this));
+		getCommand("god").setExecutor(new CommandGod(this));
 	}
 	
 	private void registerListeners(){
-		//Future listeners register
+		Bukkit.getPluginManager().registerEvents(new onJoin(this), this);
+		Bukkit.getPluginManager().registerEvents(new onEntityDamageEvent(this), this);
+	}
+	
+	public void addUser(User user){
+		if(!users.containsValue(user)){
+			users.put(user.getPlayer().getName(), user);
+		}
+	}
+	
+	public User getUser(String name){
+		return users.get(name);
 	}
 }
