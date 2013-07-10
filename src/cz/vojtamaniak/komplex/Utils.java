074d14ddfdc3411021283b9d9d05c7c5bfd5c137
vojtamaniak <vojtamaniak@gmail.com>
2013-07-10 14:01:24 +0200

    Added method buildMessage for build messages.
    (from command's variable arg)

--- src/cz/vojtamaniak/komplex/Utils.java
+++ src/cz/vojtamaniak/komplex/Utils.java
@@ -12,4 +12,13 @@ public class Utils {
 			}
 		}
 	}
+	
+	public static String buildMessage(String[] arg, int start){
+		String message = "";
+		
+		for(int i = start; i < arg.length; i++){
+			message += arg[i] + " ";
+		}
+		return message;
+	}
 }
