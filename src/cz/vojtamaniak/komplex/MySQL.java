2f82971326aa1f20c1e087511b39439451b5d26c
vojtamaniak <vojtamaniak@gmail.com>
2013-07-28 15:14:02 +0200

    Reworked MySQL

--- src/cz/vojtamaniak/komplex/MySQL.java
+++ src/cz/vojtamaniak/komplex/MySQL.java
@@ -21,7 +21,6 @@ public class MySQL {
 	private String user;
 	private String password;
 	private String database;
-	private String prefix;
 	
 	private int port;
 	
@@ -33,15 +32,9 @@ public class MySQL {
 		this.user = conf.getString("storage.mysql-user");
 		this.password = conf.getString("storage.mysql-pass");
 		this.database = conf.getString("storage.mysql-data");
-		this.prefix = conf.getString("storage.mysql-table-prefix");
-		this.port = conf.getInt("storage.mysql-port");
+		this.port = 3306;
 		
-		try {
-			this.con = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ database, user, password);
-			this.st = con.createStatement();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		open();
 	}
 	
 	public ResultSet query(String query){
@@ -63,9 +56,9 @@ public class MySQL {
 		try {
 			con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, user, password);
 			st = con.createStatement();
-			int v1 = st.executeUpdate("CREATE TABLE IF NOT EXISTS "+ prefix +"mail (id INT AUTO_INCREMENT, sender VARCHAR(32), recipient VARCHAR(32), text TEXT, time BIGINT(20), readed TINYINT(1), PRIMARY KEY (id)");
+			int v1 = st.executeUpdate("CREATE TABLE IF NOT EXISTS k_mail (id INT AUTO_INCREMENT, sender VARCHAR(32), receiver VARCHAR(32), text TEXT, time BIGINT(20), deleted TINYINT(1), PRIMARY KEY (id))");
 			if(v1 != 0){
-				log.info("[MySQL] Creating table "+prefix+"mail.");
+				log.info("[MySQL] Creating table k_mail.");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
